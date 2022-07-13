5676604a57b7708d72d61f80b14c4f4381eef0a0
kiyo66 <ogawatandf@icloud.com>
2022-07-13 18:27:11 +0900

    Final revision

--- s1290193-e4.c
+++ s1290193-e4.c
@@ -30,9 +30,9 @@ int main() {
   printf("Heads: %d, Tails: %d\n", head, tail);
 
   if(head == 2) {
-    printf("you won!\n");
+    printf("%s won!\n", name);
   } else {
-    printf("you lost\n");
+    printf("%s lost\n", name);
   }
 
   return 0;
